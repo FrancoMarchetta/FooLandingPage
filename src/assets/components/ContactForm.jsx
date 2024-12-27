233eebe4d510b2f6b4f5db47266329ad676a85b6
FrancoMarchetta <frncmarchetta@gmail.com>
2024-12-27 16:58:27 -0300

    can send emails from the contact page

--- src/assets/components/ContactForm.jsx
+++ src/assets/components/ContactForm.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import "../css/Form.css";
-
+import emailjs from "@emailjs/browser";
 const ContactForm = () => {
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        alert("submited");
+        emailjs.sendForm("service_zdebejf", "template_ac20z2m", e.target, "LJdNbJzB72B0ya1b6");
+    }
+
     return (
         <>
             <div className="container">
                 <div className="form_area">
                     <p className="title">Send us a message</p>
-                    <form action="">
-                        <div className="form_group">
-                            <label className="sub_title" htmlFor="name">Name</label>
-                            <input placeholder="Enter your full name" className="form_style" type="text" />
-                        </div>
+                    <form onSubmit={sendEmail}>
                         <div className="form_group">
                             <label className="sub_title" htmlFor="email">Email</label>
-                            <input placeholder="Enter your email" id="email" className="form_style" type="email" />
+                            <input name='emailFrom' placeholder="Enter your email" id="email" className="form_style" type="email" />
                         </div>
                         <div className="form_group">
                             <label className="sub_title" htmlFor="message">Message</label>
-                            <input placeholder="Enter your message" className="form_style" type="text" />
+                            <input name='message' placeholder="Enter your message" className="form_style" type="text" />
                         </div>
                         <div>
-                            <button className="btn">Send</button>
+                            <button type='submit' className="btn">Send</button>
                         </div>
                     </form>
                 </div>
