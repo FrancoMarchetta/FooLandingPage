657ded1536e0ba98d733019302658072c38b5891
FrancoMarchetta <frncmarchetta@gmail.com>
2024-12-29 17:30:09 -0300

    header improvement (Very Proud)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from './assets/components/Header'
 import Contact from './assets/pages/Contact';
 import MainPage from './assets/pages/MainPage'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shop from './assets/pages/Shop';
+import Menu from './assets/pages/Menu';
 
 
 
@@ -15,7 +17,9 @@ function App() {
         <Header></Header>
         <Routes>
           <Route path='/' element={<div style={{ margin: "0% 0% 0% 8%" }}><MainPage></MainPage></div>}></Route>
-          <Route path='contact' element={<Contact></Contact>}></Route>
+          <Route path='/contact' element={<Contact></Contact>}></Route>
+          <Route path='/shop' element={<Shop></Shop>}></Route>
+          <Route path='/menu' element={<Menu></Menu>}></Route>
         </Routes>
       </BrowserRouter >
     </>
--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -11,20 +11,32 @@ const Header = () => {
     const goToHome = () => {
         path("/")
     }
+    const goToMenu = () => {
+        path("/menu")
+    }
+    const goToShop = () => {
+        path("/shop")
+    }
+
+
+    console.log(window.location.pathname);
+
+
+
 
     return (
         <>
             <header className='header' >
-                <div id='foo-and-image'>
+                <div style={{ cursor: "pointer" }} onClick={goToHome} id='foo-and-image'>
                     <img src="../../public/chef-hat (2) 1.svg" alt="" />
                     <h1>Foo</h1>
                 </div>
 
                 <div className='header-options'>
-                    <p className='headerPaths' onClick={goToHome}>Home</p>
-                    <p className='headerPaths'>Menu</p>
-                    <p className='headerPaths' onClick={goToContact}>Contact</p>
-                    <p className='headerPaths'>Shop</p>
+                    {window.location.pathname == "/" ? <p className='headerPaths headerPathsUnderlined' onClick={goToHome}>Home</p> : <p className='headerPaths ' onClick={goToHome}>Home</p>}
+                    {window.location.pathname == "/contact" ? <p className='headerPaths headerPathsUnderlined' onClick={goToContact}>Contact</p> : <p className='headerPaths ' onClick={goToContact}>Contact</p>}
+                    {window.location.pathname == "/menu" ? <p className='headerPaths headerPathsUnderlined' onClick={goToMenu} >Menu</p> : <p className='headerPaths ' onClick={goToMenu}>Menu</p>}
+                    {window.location.pathname == "/shop" ? <p className='headerPaths headerPathsUnderlined' onClick={goToShop} >Shop</p> : <p className='headerPaths ' onClick={goToShop}>Shop</p>}
                     <input type="text" placeholder='Search' />
                 </div>
             </header>
